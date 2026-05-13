a65d97014f601c87783ec5604de59ebcba5d34be
Bruno Ochoa <0896045@MY.SCCCD.EDU>
2026-05-12 22:37:01 -0700

    Added Description of what this program will do as well as the directives. <iostream> is a standard in most programs, <string> for allowing words and names to be input, and <fstream> for the saving and loading feature.

---
+++ Github_Final_Project.cpp
@@ -0,0 +1,18 @@
+// CSCI-40 Final Project
+// Author: [Bruno Ochoa]
+
+/*
+ * Description: My project is a character tracker for Dungeons and Dragons or simply for character creation.
+ * This program will allow users to keep track of information such as stats, experience, inventory, character backgrounds, and more.
+ * Users will be able to update or change any information as they please at any time as well.
+ * With a save and loading feature, userswill be able to come back to their characters as they see fit.
+ * The program helps with keeping track of all this information as well as allowing the user to add buffs or debuffs to give as much creative expression and ease of use as possible.
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+    return 0;
+}
